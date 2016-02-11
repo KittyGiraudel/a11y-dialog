9051bf67161d45f2fbc3066b0e3762b3842f7b6b
Hugo Giraudel <hugo.giraudel@gmail.com>
2016-02-11 13:01:22 +0100

    Fixed a few linting errors

--- index.js
+++ index.js
@@ -83,7 +83,7 @@
     instance.$modal.addEventListener('click', function (event) {
       event.stopPropagation();
     });
-  };
+  }
 
   // Helper function to focus first focusable item in node
   function setFocusToFirstItem (node) {
@@ -107,7 +107,7 @@
     this.$closers = $$('[data-hide-modal]', this.$modal);
     this.$openers = $$('[data-show-modal="' + this.$modal.id + '"]');
     this.shown = false;
-    
+
     bindListeners(this);
   };
 
@@ -120,7 +120,7 @@
     this.$main.setAttribute('aria-hidden', 'true');
     this.$modal.setAttribute('aria-hidden', 'false');
     this.$overlay.style.display = 'block';
-    this.$modal.style.display = 'block'; 
+    this.$modal.style.display = 'block';
 
     focusedElementBeforeModal = document.activeElement;
     setFocusToFirstItem(this.$modal);
@@ -140,5 +140,5 @@
     focusedElementBeforeModal.focus();
   };
 
-  global.Modal = Modal
+  global.Modal = Modal;
 }(window));

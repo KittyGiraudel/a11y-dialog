fecae0d5daaa4688bd762aabaf2767b75e947315
Hugo Giraudel <hugo.giraudel@gmail.com>
2016-02-16 16:42:11 +0100

    Restructured test suite

--- tests/index.js
+++ tests/index.js
@@ -2,52 +2,82 @@
 
 var modalID = 'my-accessible-modal';
 
-casper.test.begin('Modal module tests', 24, function suite (test) {
-
-  casper.start('http://0.0.0.0:4444', function () {
-    casper.page.injectJs('./accessible-modal-dialog.js');
-    casper.page.onConsoleMessage = console.log.bind(console, 'DEBUG: ');
-    casper.page.evaluateJavaScript('(function () { new window.Modal(document.getElementById("' + modalID + '")) }())');
-    this.emit('page.loaded');
-  });
+casper.test.begin('Modal test suite', 26, function (test) {
 
   function testAriaHidden (isModalOpen) {
     var modal = '#' + modalID;
 
-    test.assertExist('#main[aria-hidden="' + isModalOpen + '"]');
-    test.assertDoesntExist('#main[aria-hidden="' + !isModalOpen + '"]');
-    test.assertExist(modal + '[aria-hidden="' + !isModalOpen + '"]');
-    test.assertDoesntExist(modal + '[aria-hidden="' + isModalOpen + '"]');
+    test.assertExist('#main[aria-hidden="' + isModalOpen + '"]', 'Main element has `aria-hidden="' + isModalOpen + '"`');
+    test.assertDoesntExist('#main[aria-hidden="' + !isModalOpen + '"]', 'Main element has `aria-hidden="' + !isModalOpen + '"`');
+    test.assertExist(modal + '[aria-hidden="' + !isModalOpen + '"]', 'Modal element has `aria-hidden="' + !isModalOpen + '"`');
+    test.assertDoesntExist(modal + '[aria-hidden="' + isModalOpen + '"]', 'Modal element has `aria-hidden="' + isModalOpen + '"`');
   }
 
-  casper.on('page.loaded', function () {
+  casper.start('http://edenspiekermann.github.io/accessible-modal-dialog/', function () {
+    casper.page.onConsoleMessage = function (msg, lineNum, sourceId) {
+      console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
+    };
+
+    casper.page.injectJs('./accessible-modal-dialog.js');
+    casper.page.evaluateJavaScript('function () { window.m = new window.Modal(document.getElementById("' + modalID + '")); }');
+
     var modal = '#' + modalID;
     var opener = '[data-modal-show="' + modalID + '"]';
     var closer = modal + ' [data-modal-hide]';
     var overlay = modal + ' > .modal-overlay';
 
-    // Test initial setup
-    test.assertEvalEquals(function () { return typeof window.Modal; }, 'function', 'Modal is being defined');
-    test.assertExist(modal, 'Modal element exists in the DOM');
-    test.assertExist(opener, 'Modal opener element exists in the DOM');
-    test.assertNotVisible(modal, 'Modal is hidden by default');
-    testAriaHidden(false);
+    casper.then(function () {
+      this.echo('');
+      this.echo('Test Modal shape');
+      test.assertEvalEquals(function () { return typeof window.Modal; }, 'function', 'Modal constructor is being defined');
+      test.assertEvalEquals(function () { return typeof m.hide; }, 'function', 'Modal instance has a `hide` method');
+      test.assertEvalEquals(function () { return typeof m.show; }, 'function', 'Modal instance has a `show` method');
+    });
 
-    // Test modal opening
-    this.click(opener), testAriaHidden(true);
+    casper.then(function () {
+      this.echo('');
+      this.echo('Test initial setup');
+      test.assertExist(modal, 'Modal element exists in the DOM');
+      test.assertExist(opener, 'Modal opener element exists in the DOM');
+      test.assertNotVisible(modal, 'Modal is hidden by default');
+      testAriaHidden(false);
+    });
 
-    // Test modal closing through overlay
-    this.click(overlay), testAriaHidden(false);
+    casper.then(function () {
+      this.echo('');
+      this.echo('Test modal opening');
+      casper.click(opener);
+      testAriaHidden(true);
+    });
 
-    // Test modal closing through closer
-    this.click(opener), this.click(closer), testAriaHidden(false);
+    casper.then(function () {
+      this.echo('');
+      this.echo('Test modal closing through overlay');
+      casper.click(overlay);
+      testAriaHidden(false);
+    });
 
-    // Test modal closing through ESCAPE key
-    this.click(opener), casper.page.sendEvent('keypress', casper.page.event.key.Escape, null, null, 0), testAriaHidden(false);
+    casper.then(function () {
+      this.echo('');
+      this.echo('Test modal closing through closer');
+      casper.click(opener);
+      casper.click(closer);
+      testAriaHidden(false);
+    });
+
+    casper.then(function () {
+      this.echo('');
+      this.echo('Test modal closing through ESCAPE key');
+      casper.click(opener);
+      casper.page.sendEvent('keypress', casper.page.event.key.Escape, null, null, 0);
+      testAriaHidden(false);
+    });
+
   });
 
   casper.run(function () {
-    test.done();
+    this.test.done();
+    this.exit();
   });
 
 });

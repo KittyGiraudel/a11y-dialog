995c8231c9aa9f59c5e8f3aa732e3f4fcbdb989b
Brian Teeman <brian@teeman.net>
2018-10-07 08:38:46 +0100

    English corrections
    Just a small correction to the english grammar

--- a11y-dialog.js
+++ a11y-dialog.js
@@ -176,14 +176,14 @@
       this.dialog.removeAttribute('open');
       this.container.setAttribute('aria-hidden', 'true');
 
-      // Iterate over the targets to enable them by remove their `aria-hidden`
+      // Iterate over the targets to enable them by removing their `aria-hidden`
       // attribute
       this._targets.forEach(function(target) {
         target.removeAttribute('aria-hidden');
       });
     }
 
-    // If their was a focused element before the dialog was opened, restore the
+    // If there was a focused element before the dialog was opened, restore the
     // focus back to it
     if (focusedBeforeDialog) {
       focusedBeforeDialog.focus();

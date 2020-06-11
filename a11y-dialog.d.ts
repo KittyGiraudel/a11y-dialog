5d477feaf8246180c34afa7ade06e3eff3087ee6
Moritz Kröger <write@morkro.de>
2020-06-11 17:45:37 +0200

    Fixes wrong name and adds more specificity for event names

--- a11y-dialog.d.ts
+++ a11y-dialog.d.ts
@@ -1,11 +1,12 @@
 declare namespace A11yDialog {
   export type Targets = NodeList | Element | string;
   export type EventHandler = (node: Element, event?: Event) => void;
+  export type EventType = "show" | "hide" | "destroy" | "create";
 
   export class A11yDialog {
     constructor(node: Element, targets?: Targets);
 
-    create(targets: DialogTargets): A11yDialog;
+    create(targets: Targets): A11yDialog;
 
     show(event: Event): A11yDialog;
 
@@ -13,11 +14,11 @@ declare namespace A11yDialog {
 
     destroy(): A11yDialog;
 
-    on(type: string, handler: EventHandler): A11yDialog;
+    on(type: EventType, handler: EventHandler): A11yDialog;
 
-    off(type: string, handler: EventHandler): A11yDialog;
+    off(type: EventType, handler: EventHandler): A11yDialog;
 
-    private _fire(type: string, event: Event): void;
+    private _fire(type: EventType, event: Event): void;
 
     private _bindKeypress(event: Event): void;
 

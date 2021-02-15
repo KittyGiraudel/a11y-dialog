510f6aca33de3348e3277392fe023e5e954c3193
Kitty Giraudel <hugo.giraudel@gmail.com>
2021-02-15 13:31:29 +0100

    Clean up rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,43 +1,23 @@
 import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
+const umd = { format: 'umd', name: 'A11yDialog', exports: 'default' }
+const es = { format: 'es' }
+const minify = {
+  plugins: [terser()],
+  banner: () => `/*! a11y-dialog ${pkg.version} — © Edenspiekermann */`,
+}
+
 export default {
   input: 'a11y-dialog.js',
   output: [
-    // Main file
-    {
-      file: 'dist/a11y-dialog.js',
-      format: 'umd',
-      name: 'A11yDialog',
-      exports: 'default',
-    },
-    // Minified version
-    {
-      file: 'dist/a11y-dialog.min.js',
-      format: 'umd',
-      name: 'A11yDialog',
-      exports: 'default',
-      plugins: [terser()],
-      banner: () => `/*! a11y-dialog ${pkg.version} — © Edenspiekermann */`,
-    },
+    // Main files
+    { file: 'dist/a11y-dialog.js', ...umd },
+    { file: 'dist/a11y-dialog.esm.js', ...es },
+    // Minified versions
+    { file: 'dist/a11y-dialog.min.js', ...umd, ...minify },
+    { file: 'dist/a11y-dialog.esm.min.js', ...es, ...minify },
     // Copy of the main file for example and tests
-    {
-      file: 'example/a11y-dialog.js',
-      format: 'umd',
-      name: 'A11yDialog',
-      exports: 'default',
-    },
-    // ESM version for <script type="module"> support
-    {
-      file: 'dist/a11y-dialog.esm.js',
-      format: 'es',
-    },
-    // Minified ESM version
-    {
-      file: 'dist/a11y-dialog.esm.min.js',
-      format: 'es',
-      plugins: [terser()],
-      banner: () => `/*! a11y-dialog ${pkg.version} — © Edenspiekermann */`,
-    },
+    { file: 'example/a11y-dialog.js', ...umd },
   ],
 }

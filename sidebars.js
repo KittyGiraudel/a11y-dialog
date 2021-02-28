1813b7737838922cb6ed86c3a8e69e194e29aeda
Kitty Giraudel <hugo.giraudel@gmail.com>
2021-02-28 16:33:26 +0100

    Split interaction docs into its own page

--- sidebars.js
+++ sidebars.js
@@ -1,38 +1,43 @@
 module.exports = {
   docs: [
     {
-      type: "doc",
-      id: "introduction",
+      type: 'doc',
+      id: 'introduction',
     },
     {
-      type: "doc",
-      id: "installation",
+      type: 'doc',
+      id: 'installation',
     },
     {
-      type: "category",
-      label: "Usage",
-      items: ["usage.markup", "usage.styling", "usage.instantiation"],
-    },
-    {
-      type: "category",
-      label: "Advanced",
+      type: 'category',
+      label: 'Usage',
       items: [
-        "advanced.focus_considerations",
-        "advanced.animations",
-        "advanced.alert_dialog",
-        "advanced.events",
-        "advanced.scroll_lock",
-        "advanced.dialog_element",
-        "advanced.nested_dialogs",
+        'usage.markup',
+        'usage.styling',
+        'usage.instantiation',
+        'usage.interactions',
       ],
     },
     {
-      type: "category",
-      label: "Further reading",
+      type: 'category',
+      label: 'Advanced',
       items: [
-        "further_reading.implementations",
-        "further_reading.known_issues",
+        'advanced.focus_considerations',
+        'advanced.animations',
+        'advanced.alert_dialog',
+        'advanced.events',
+        'advanced.scroll_lock',
+        'advanced.dialog_element',
+        'advanced.nested_dialogs',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Further reading',
+      items: [
+        'further_reading.implementations',
+        'further_reading.known_issues',
       ],
     },
   ],
-};
+}

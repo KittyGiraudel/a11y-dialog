b3e57b385bdc736d201baade3a94e6122a752e3d
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-06-09 10:07:13 +0200

    Prepare version 7.5.0

--- cypress/fixtures/a11y-dialog.js
+++ cypress/fixtures/a11y-dialog.js
@@ -19,8 +19,8 @@
     '[tabindex]:not([tabindex^="-"])',
   ];
 
-  var TAB_KEY = 9;
-  var ESCAPE_KEY = 27;
+  var TAB_KEY = 'Tab';
+  var ESCAPE_KEY = 'Escape';
 
   /**
    * Define the constructor to instantiate a dialog
@@ -262,7 +262,7 @@
     // is 'alertdialog', which should be modal
     if (
       this.shown &&
-      event.which === ESCAPE_KEY &&
+      event.key === ESCAPE_KEY &&
       this.$el.getAttribute('role') !== 'alertdialog'
     ) {
       event.preventDefault();
@@ -271,7 +271,7 @@
 
     // If the dialog is shown and the TAB key is being pressed, make sure the
     // focus stays trapped within the dialog element
-    if (this.shown && event.which === TAB_KEY) {
+    if (this.shown && event.key === TAB_KEY) {
       trapTabKey(this.$el, event);
     }
   };

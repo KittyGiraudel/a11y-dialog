0e7691fcb715b7d4c8e0c008262672660b89fac2
EJ Mason <13525251+mxmason@users.noreply.github.com>
2022-06-30 19:21:49 -0700

    chore: rewarite rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,59 +2,92 @@ import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import { getBabelOutputPlugin } from '@rollup/plugin-babel'
+import { babel } from '@rollup/plugin-babel'
 
-const umd = {
-  format: 'es',
+const babelCfg = {
+  assumptions: {
+    enumerableModuleMeta: true,
+    mutableTemplateObject: true,
+    noClassCalls: true,
+    noDocumentAll: true,
+  },
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: 'ie 11',
+      },
+    ],
+  ],
+}
+
+const umdCfg = {
+  format: 'umd',
   name: 'A11yDialog',
   exports: 'default',
-  plugins: [
-    getBabelOutputPlugin({
-      assumptions: {
-        enumerableModuleMeta: true,
-        mutableTemplateObject: true,
-        noClassCalls: true,
-        noDocumentAll: true,
-      },
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            modules: ['umd'],
-            targets: 'ie 11',
-            exclude: ['proposal-dynamic-import', 'transform-modules-commonjs'],
-          },
-        ],
-      ],
-    }),
-  ],
 }
-const es = { format: 'es' }
-const minify = (plugins = []) => ({
-  plugins: [
-    ...plugins,
-    terser({
-      format: {
-        preamble: `/*! a11y-dialog ${pkg.version} — © Kitty Giraudel */`,
-      },
-    }),
-  ],
+
+const commonPlugins = [
+  nodeResolve(),
+  commonjs({ include: 'node_modules/**' }),
+  babel(babelCfg),
+]
+
+const minify = terser({
+  format: {
+    preamble: `/*! a11y-dialog ${pkg.version} — © Kitty Giraudel */`,
+  },
 })
 
-export default {
-  input: 'src/a11y-dialog.js',
-  output: [
-    // Main files
-    {
-      file: 'dist/a11y-dialog.js',
-      ...umd,
-    },
-    { file: 'dist/a11y-dialog.esm.js', ...es },
-    // Minified versions
-    { file: 'dist/a11y-dialog.min.js', ...umd, ...minify(umd.plugins) },
-    { file: 'dist/a11y-dialog.esm.min.js', ...es, ...minify() },
-    // Test version
-    { file: 'cypress/fixtures/a11y-dialog.js', ...umd },
-  ],
-  plugins: [nodeResolve(), commonjs({ include: 'node_modules/**' })],
-}
+export default [
+  {
+    input: 'src/a11y-dialog.js',
+    output: [
+      {
+        file: 'dist/a11y-dialog.js',
+        ...umdCfg,
+      },
+    ],
+    plugins: commonPlugins,
+  },
+  {
+    input: 'src/a11y-dialog.js',
+    output: [
+      {
+        file: 'dist/a11y-dialog.min.js',
+        ...umdCfg,
+      },
+    ],
+    plugins: [...commonPlugins, minify],
+  },
+  {
+    input: 'src/a11y-dialog.js',
+    output: [
+      {
+        file: 'dist/a11y-dialog.esm.js',
+        format: 'esm',
+      },
+    ],
+    plugins: commonPlugins,
+  },
+  {
+    input: 'src/a11y-dialog.js',
+    output: [
+      {
+        file: 'dist/a11y-dialog.esm.min.js',
+        format: 'esm',
+      },
+    ],
+    plugins: [...commonPlugins, minify],
+  },
+  {
+    input: 'src/a11y-dialog.js',
+    output: [
+      {
+        file: 'cypress/fixtures/a11y-dialog.js',
+        ...umdCfg,
+      },
+    ],
+    plugins: commonPlugins,
+  },
+]

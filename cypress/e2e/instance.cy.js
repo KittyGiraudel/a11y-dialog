ea2bb71430c2da822ec7b6572b0e8d5dd7b9dc8d
Kitty Giraudel <kitty.giraudel@gmail.com>
2022-07-27 16:05:55 +0200

    Consolidate event-related tests

--- cypress/e2e/instance.cy.js
+++ cypress/e2e/instance.cy.js
@@ -6,8 +6,7 @@ describe('Instance', () => {
   it('should be possible to instantiate a dialog with JavaScript', () => {
     cy.window().then(win => {
       win.instance = new win.A11yDialog(
-        win.document.getElementById('my-dialog'),
-        'main'
+        win.document.getElementById('my-dialog')
       )
     })
   })
@@ -35,38 +34,77 @@ describe('Instance', () => {
   })
 
   it('should be possible to register/unregister events', () => {
-    let logs = []
-
-    function event(container) {
-      logs.push('Should Not Fire')
+    const handlers = {
+      show: event => {
+        // When programmatically showing the dialog, event details are not set.
+        expect(event.detail).to.eq(null)
+        expect(event.target.id).to.eq('my-dialog')
+      },
+      showManual: event => {
+        // When manually showing the dialog, event details should contain the
+        // opener.
+        expect(
+          event.detail.currentTarget.getAttribute('data-a11y-dialog-show')
+        ).to.eq('my-dialog')
+        expect(event.target.id).to.eq('my-dialog')
+      },
+      hide: event => {
+        // When programmatically hiding the dialog, event details are not set.
+        expect(event.detail).to.eq(null)
+        expect(event.target.id).to.eq('my-dialog')
+      },
+      hideManual: event => {
+        // When manually showing the dialog, event details should contain the
+        // closeer.
+        expect(event.detail.currentTarget.hasAttribute('data-a11y-dialog-hide'))
+        expect(event.target.id).to.eq('my-dialog')
+      },
+      destroy: event => {
+        expect(event.target.id).to.eq('my-dialog')
+      },
     }
 
+    // Spy on handlers to know whether they’ve been called
+    cy.spy(handlers, 'show').as('show')
+    cy.spy(handlers, 'hide').as('hide')
+    cy.spy(handlers, 'showManual').as('showManual')
+    cy.spy(handlers, 'hideManual').as('hideManual')
+    cy.spy(handlers, 'destroy').as('destroy')
+
+    // Register event listeners on show, hide and destroy events
     cy.window()
       .its('instance')
+      .invoke('on', 'show', handlers.show)
+      .invoke('on', 'hide', handlers.hide)
+      .invoke('on', 'destroy', handlers.destroy)
 
-      .invoke('on', 'show', event => {
-        expect(event.target.id).to.eq('my-dialog')
-        logs.push('Shown')
-      })
+    // Programmatically show the dialog and ensure the show handler has been
+    // called
+    cy.window().its('instance').invoke('show')
+    cy.get('@show').should('have.been.called')
 
-      .invoke('on', 'hide', event => {
-        expect(event.target.id).to.eq('my-dialog')
-        logs.push('Hidden')
-      })
+    // Programmatically hide the dialog and ensure the hide handler has been
+    // called
+    cy.window().its('instance').invoke('hide')
+    cy.get('@hide').should('have.been.called')
 
-      .invoke('on', 'hide', event)
-      .invoke('off', 'hide', event)
+    // Replace the show handler with one that test for a manual action, and
+    // manually show the dialog to ensure it was called properly
+    cy.window().its('instance').invoke('off', 'show', handlers.show)
+    cy.window().its('instance').invoke('on', 'show', handlers.showManual)
+    cy.get('[data-a11y-dialog-show="my-dialog"]').click()
+    cy.get('@showManual').should('have.been.called')
 
-      .invoke('on', 'destroy', event => {
-        expect(event.target.id).to.eq('my-dialog')
-        logs.push('Destroyed')
-      })
+    // Replace the hide handler with one that test for a manual action, and
+    // manually hide the dialog to ensure it was called properly
+    cy.window().its('instance').invoke('off', 'hide', handlers.hide)
+    cy.window().its('instance').invoke('on', 'hide', handlers.hideManual)
+    cy.get('[data-a11y-dialog-hide').last().click()
+    cy.get('@hideManual').should('have.been.called')
 
-      .invoke('show')
-      .invoke('hide')
-      .invoke('destroy')
-
-    cy.wrap(logs).should('deep.equal', ['Shown', 'Hidden', 'Destroyed'])
+    // Destroy the dialog and ensure the destroy handler has been called
+    cy.window().its('instance').invoke('destroy')
+    cy.get('@destroy').should('have.been.called')
   })
 
   it('should be possible to handle dialog destroy', () => {

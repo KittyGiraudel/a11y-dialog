825c54c352225fb3b044bfcdcd8583c89567cd09
EJ Mason <13525251+mxmason@users.noreply.github.com>
2023-01-25 14:01:39 -0800

    chore: write basic Light DOM tests

--- cypress/e2e/getFocusableEdges.cy.js
+++ cypress/e2e/getFocusableEdges.cy.js
@@ -12,4 +12,23 @@ describe('getFocusableEdges()', { testIsolation: false }, () => {
       expect(focusableEdges[1]).to.not.be.null
     })
   })
+  it('Should return the same element twice if there is only one focusable element', () => {
+    cy.get('#light-dom-one-el').then(container => {
+      const focusableEdges = getFocusableEdges(container[0])
+
+      expect(focusableEdges).to.have.length(2)
+      expect(focusableEdges[0]).to.not.be.null
+      expect(focusableEdges[1]).to.not.be.null
+      expect(focusableEdges[0]).to.equal(focusableEdges[1])
+    })
+  })
+  it('Should return null if there are no focusable elements', () => {
+    cy.get('#light-dom-no-els').then(container => {
+      const focusableEdges = getFocusableEdges(container[0])
+
+      expect(focusableEdges).to.have.length(2)
+      expect(focusableEdges[0]).to.be.null
+      expect(focusableEdges[1]).to.be.null
+    })
+  })
 })

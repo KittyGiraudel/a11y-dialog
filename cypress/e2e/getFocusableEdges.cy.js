b6e1a14409e86d67ec29338c0dde109859f0e84c
EJ Mason <13525251+mxmason@users.noreply.github.com>
2023-01-25 21:25:16 -0800

    chore: clean up

--- cypress/e2e/getFocusableEdges.cy.js
+++ cypress/e2e/getFocusableEdges.cy.js
@@ -21,10 +21,9 @@ describe('getFocusableEdges()', { testIsolation: false }, () => {
   it('should return two unique elements if multiple focusable elements are present', () => {
     cy.get('#light-dom-two-els').then(container => {
       const elems = getFocusableEdges(container[0])
-      expect(elems).to.have.length(2)
-
       const [first, last] = elems
 
+      expect(elems).to.have.length(2)
       expect(isElement(first)).to.be.true
       expect(isElement(last)).to.be.true
       expect(first).to.not.equal(last)
@@ -35,20 +34,22 @@ describe('getFocusableEdges()', { testIsolation: false }, () => {
   it('should return the same element twice if there is only one focusable element', () => {
     cy.get('#light-dom-one-el').then(container => {
       const elems = getFocusableEdges(container[0])
+      const [first, last] = elems
 
       expect(elems).to.have.length(2)
-      expect(elems[0]).to.not.be.null
-      expect(elems[1]).to.not.be.null
-      expect(elems[0]).to.equal(elems[1])
+      expect(first).to.not.be.null
+      expect(last).to.not.be.null
+      expect(first).to.equal(last)
     })
   })
   it('should return [null, null] if there are no focusable elements', () => {
     cy.get('#light-dom-no-els').then(container => {
       const elems = getFocusableEdges(container[0])
+      const [first, last] = elems
 
       expect(elems).to.have.length(2)
-      expect(elems[0]).to.be.null
-      expect(elems[1]).to.be.null
+      expect(first).to.be.null
+      expect(last).to.be.null
     })
   })
   it('should return Shadow DOM elements', () => {
@@ -66,14 +67,11 @@ describe('getFocusableEdges()', { testIsolation: false }, () => {
   it('should return slotted Light DOM elements and elements nested in Shadow DOM', () => {
     cy.get('#shadow-dom-mixed').then(container => {
       const elems = getFocusableEdges(container[0])
-
-      expect(elems).to.have.length(2)
-
       const [first, last] = elems
 
+      expect(elems).to.have.length(2)
       expect(first).to.not.be.null
       expect(last).to.not.be.null
-
       // The first focusable element is slotted, so it has no host
       expect(isInShadow(first)).to.be.false
       expect(first.id).to.equal('slotted-link')
@@ -96,20 +94,22 @@ describe('getFocusableEdges()', { testIsolation: false }, () => {
   it('should ignore nodes in unfocusable subtrees', () => {
     cy.get('#inert-children').then(container => {
       const elems = getFocusableEdges(container[0])
+      const [first, last] = elems
 
       expect(elems).to.have.length(2)
       // The child of the inert div
-      expect(elems[0]).to.be.null
+      expect(first).to.be.null
       // The individially disabled button
-      expect(elems[1]).to.be.null
+      expect(last).to.be.null
     })
     cy.get('#hidden-children').then(container => {
       const elems = getFocusableEdges(container[0])
+      const [first, last] = elems
 
       // The only focusable element is the child of a hidden div
       expect(elems).to.have.length(2)
-      expect(elems[0]).to.be.null
-      expect(elems[1]).to.be.null
+      expect(first).to.be.null
+      expect(last).to.be.null
     })
   })
 })

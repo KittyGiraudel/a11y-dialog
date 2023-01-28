60394dd54dd6fffcfe47daa107289572cbedde6e
Kitty Giraudel <kitty.giraudel@gmail.com>
2023-01-28 15:49:03 +0100

    Add global styles to improve rendering

--- cypress.config.js
+++ cypress.config.js
@@ -5,4 +5,13 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5000',
   },
+  env: {
+    'cypress-fiddle': {
+      style: `
+        *:not(pre *) { font-family: sans-serif; }
+        body { max-width: 600px; margin: 1em auto; }
+        h1 { font-size: 150% }
+      `,
+    },
+  },
 })

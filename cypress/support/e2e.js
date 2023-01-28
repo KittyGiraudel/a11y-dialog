f774342650711e3d97b513079527e8f9389a8923
Kitty Giraudel <kitty.giraudel@gmail.com>
2023-01-28 14:26:37 +0100

    Add a custom command to get the focusable edges

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,13 @@
 import 'cypress-real-events/support'
 import '@cypress/fiddle'
+import { getFocusableEdges } from '../fixtures/dom-utils'
+
+Cypress.Commands.add('getFocusableEdges', { prevSubject: true }, subject => {
+  cy.wrap(subject, { log: false })
+    .then(subject => getFocusableEdges(subject[0]))
+    .as('edges')
+    .should('have.length', 2)
+
+  cy.get('@edges', { log: false }).its('0', { log: false }).as('first')
+  cy.get('@edges', { log: false }).its('1', { log: false }).as('last')
+})

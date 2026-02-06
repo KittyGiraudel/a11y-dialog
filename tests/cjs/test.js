9ab7ae297fc6fb43b5fa6978e8a30d94c0b4a779
Kitty Giraudel <kitty.giraudel@gmail.com>
2026-02-06 14:07:36 +0100

    Improve coverage of node tests

--- tests/cjs/test.js
+++ tests/cjs/test.js
@@ -2,3 +2,13 @@ const A11yDialog = require('a11y-dialog')
 const assert = require('node:assert')
 
 assert.ok(A11yDialog, 'a11y-dialog can be used via require')
+assert.strictEqual(
+  typeof A11yDialog,
+  'function',
+  'a11y-dialog CommonJS export is a constructor/function'
+)
+assert.strictEqual(
+  typeof A11yDialog.prototype.show,
+  'function',
+  'a11y-dialog instances expose a show method'
+)
--- tests/esm/test.js
+++ tests/esm/test.js
@@ -2,3 +2,13 @@ import assert from 'node:assert'
 import A11yDialog from 'a11y-dialog'
 
 assert.ok(A11yDialog, 'a11y-dialog can be used via import')
+assert.strictEqual(
+  typeof A11yDialog,
+  'function',
+  'a11y-dialog default export is a constructor/function'
+)
+assert.strictEqual(
+  typeof A11yDialog.prototype.show,
+  'function',
+  'a11y-dialog instances expose a show method'
+)
